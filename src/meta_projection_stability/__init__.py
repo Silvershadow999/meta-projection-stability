8beff43bb12c61811bb88e0cf534d925b5c879ca
Silvershadow999 <nicole-drinda@hotmail.de>
2026-02-23 19:09:14 +0100

    Update __init__.py

--- src/meta_projection_stability/__init__.py
+++ src/meta_projection_stability/__init__.py
@@ -1,52 +1,34 @@
 from .config import MetaProjectionStabilityConfig
 from .adapter import MetaProjectionStabilityAdapter
 
+from .governor import (
+    GovernorState,
+    compute_autonomy_index,
+    determine_mode,
+    governor_step,
+    governor_step_from_config,
+)
+
+from .integrity_barometer import (
+    IntegrityBarometerConfig,
+    IntegrityBarometerState,
+    barometer_step,
+)
+
 __version__ = "0.1.0-dev"
 
-# Basis-Exports (immer verfügbar)
 __all__ = [
+    # Core
     "MetaProjectionStabilityConfig",
     "MetaProjectionStabilityAdapter",
+    # Governor
+    "GovernorState",
+    "compute_autonomy_index",
+    "determine_mode",
+    "governor_step",
+    "governor_step_from_config",
+    # Integrity barometer
+    "IntegrityBarometerConfig",
+    "IntegrityBarometerState",
+    "barometer_step",
 ]
-
-# Optionale Governor-API (nur exportieren, wenn vorhanden)
-try:
-    from .governor import (
-        GovernorState,
-        governor_step,
-        compute_autonomy_index,
-        determine_mode,
-        summarize_governor_history,
-        compute_trinity_autonomy_histories,
-    )
-
-    __all__ += [
-        "GovernorState",
-        "governor_step",
-        "compute_autonomy_index",
-        "determine_mode",
-        "summarize_governor_history",
-        "compute_trinity_autonomy_histories",
-    ]
-except Exception:
-    # Governor ist optional / evtl. ältere Version im Repo
-    pass
-
-# Optionale Integrity-Barometer-API (nur exportieren, wenn vorhanden)
-try:
-    from .integrity_barometer import (
-        IntegrityBarometerConfig,
-        IntegrityBarometerState,
-        barometer_step,
-        summarize_barometer_history,
-    )
-
-    __all__ += [
-        "IntegrityBarometerConfig",
-        "IntegrityBarometerState",
-        "barometer_step",
-        "summarize_barometer_history",
-    ]
-except Exception:
-    # integrity_barometer.py evtl. noch nicht lokal synchron
-    pass
