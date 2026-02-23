ec3483528b93c0063292db57a93b1c3f4cc77113
Silvershadow999 <nicole-drinda@hotmail.de>
2026-02-23 13:37:21 +0100

    Delete src/meta_projection_stability/init.py

--- src/meta_projection_stability/init.py
+++
@@ -1,8 +0,0 @@
-from .config import MetaProjectionStabilityConfig
-from .adapter import MetaProjectionStabilityAdapter
-
-__version__ = "0.1.0-dev"
-__all__ = [
-    "MetaProjectionStabilityConfig",
-    "MetaProjectionStabilityAdapter",
-]
