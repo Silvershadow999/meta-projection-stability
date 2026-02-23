ed20662c4b63a25e27780a1c8418104a16cf4d17
Silvershadow999 <nicole-drinda@hotmail.de>
2026-02-23 13:36:17 +0100

    Update __init__.py

--- src/meta_projection_stability/__init__.py
+++ src/meta_projection_stability/__init__.py
@@ -1,3 +1,6 @@
+from .config import MetaProjectionStabilityConfig
+from .adapter import MetaProjectionStabilityAdapter
+
 from .governor import (
     GovernorState,
     governor_step,
@@ -14,3 +17,21 @@ from .integrity_barometer import (
     summarize_barometer_history,
     map_barometer_to_governor_inputs,
 )
+
+__version__ = "0.1.0-dev"
+
+__all__ = [
+    "MetaProjectionStabilityConfig",
+    "MetaProjectionStabilityAdapter",
+    "GovernorState",
+    "governor_step",
+    "compute_autonomy_index",
+    "determine_mode",
+    "summarize_governor_history",
+    "IntegrityBarometerConfig",
+    "IntegrityBarometerState",
+    "barometer_step",
+    "compute_barometer_history",
+    "summarize_barometer_history",
+    "map_barometer_to_governor_inputs",
+]
